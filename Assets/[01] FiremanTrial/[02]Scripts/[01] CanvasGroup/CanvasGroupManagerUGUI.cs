23bc20d550a943808801702d2e3350a82c92f808
Manuel <manuelggarcialopes@gmail.com>
2025-01-28 20:34:36 -0300

    General scripts

---
+++ Assets/[01] FiremanTrial/[02]Scripts/[01] CanvasGroup/CanvasGroupManagerUGUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace FiremanTrial
+{
+    public class CanvasGroupManagerUGUI : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup canvasGroup;
+
+        public void MakeVisibleAndInteractive(bool value)
+        {
+            CanvasGroupManager.VisibleAndInteractive(value, canvasGroup);
+        }
+        
+        public void MakeVisible(bool value)
+        {
+            CanvasGroupManager.Visible(value, canvasGroup);
+        }
+        
+        public void MakeInteractive(bool value)
+        {
+            CanvasGroupManager.Interactive(value, canvasGroup);
+        }
+    }
+}
