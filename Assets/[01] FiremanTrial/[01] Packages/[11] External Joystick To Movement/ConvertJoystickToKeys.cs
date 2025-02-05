ead57b5a19cbc83b5f3f350ece4b624ce800fc83
Manuel <manuelggarcialopes@gmail.com>
2025-02-05 17:13:26 -0300

    Removed not used objects

---
+++ Assets/[01] FiremanTrial/[01] Packages/[11] External Joystick To Movement/ConvertJoystickToKeys.cs
@@ -0,0 +1,74 @@
+﻿using FiremanTrial.Commands;
+using UnityEngine;
+using VirtualJoystick;
+
+namespace FiremanTrial.JoystickConverter
+{
+    public class ConvertJoystickToKeys : MonoBehaviour
+    {
+        [SerializeField] private Joystick variableJoystick;
+
+        [SerializeField] private bool verticalDirection;
+        [SerializeField] private bool horizontalDirection;
+        [SerializeField] private Command positive;
+        [SerializeField] private Command positiveToZero;
+        [SerializeField] private Command negative;
+        [SerializeField] private Command negativeToZero;
+        private int _oldValue=0;
+        
+        private void OnEnable() => variableJoystick.DirectionChanged += UpdateAxes;
+
+        private void OnDisable() => variableJoystick.DirectionChanged -= UpdateAxes;
+
+        private void UpdateAxes(Vector2 joystickDirection)
+        {
+            if (verticalDirection && horizontalDirection)
+            {
+                Debug.Log("Only one direction bool is supported.", this);
+            }
+
+            if (verticalDirection)
+            {
+                Handler(joystickDirection.y);
+            }
+            else if (horizontalDirection)
+            {
+                Handler(joystickDirection.x);
+            }
+        }
+
+        private void Handler(float xValue)
+        {
+            var value= Mathf.RoundToInt(xValue);
+            if (value == _oldValue) return;
+            
+            switch (_oldValue)
+            {
+                case 0 when value == 1:
+                    positive?.Execute();
+                    break;
+                case 0 when value == -1:
+                    negative?.Execute();
+                    break;
+                case 1 when value == 0:
+                    positiveToZero?.Execute();
+                    break;
+                case 1 when value == -1:
+                    positiveToZero?.Execute();
+                    negative?.Execute();
+                    break;
+                case -1 when value == 0:
+                    negativeToZero?.Execute();
+                    break;
+                case -1 when value == 1 :
+                    negativeToZero?.Execute();
+                    positive?.Execute();
+                    break;
+            }     
+            
+            _oldValue= value;
+        }
+
+        
+    }
+}
